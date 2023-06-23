a6d4238e7f0d3678c37e592dad19a7726a45b3db
Abhijit Mishra <abhijitmishraak10@gmail.com>
2023-06-23 20:55:31 +0530

    tried to play with crud

--- src/index.js
+++ src/index.js
@@ -19,11 +19,13 @@ app.listen(3000,async ()=>{
     //                 Tweets.userEmail = 'bal@t.com';
 const TweetRepo = new TweetRepository();
 
-const tweet = await TweetRepo.create({content:'Tweet with comment Schema'});
-console.log(tweet);
-const comment = await Comments.create({content:'New comment'});
-tweet.comments.push(comment)
-await tweet.save();
+// const tweet = await TweetRepo.create({content:'Tweet with comment Schema'});
+// console.log(tweet);
+// const comment = await Comments.create({content:'New comment'});
+// tweet.comments.push(comment)
+// await tweet.save();
+// const tweet = await TweetRepo.get('6495b515d9f16243bfb9cee2');
+const tweet = await TweetRepo.update('6495951af38345441c52aac4',{userEmail:'kaka@gmail.com'})
 console.log(tweet)
 
 })
