14a3c493e4d5b022c64b4da9cc71e7a334ab6280
Abhijit Mishra <abhijitmishraak10@gmail.com>
2023-06-23 17:52:35 +0530

    basic connection to mongo db

---
+++ src/index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const connect = require('./config/database')
+const app = express();
+
+app.listen(3000,async ()=>{
+    console.log('Server Started');
+    await connect();
+    console.log('MongoDb connected');
+
+})
