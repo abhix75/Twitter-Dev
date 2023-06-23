60696872eacdc5abf685edd450bd2eef79a2673d
Abhijit Mishra <abhijitmishraak10@gmail.com>
2023-06-23 18:25:30 +0530

    added the twitter models and created some tweets

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 const express = require('express');
 const connect = require('./config/database')
 const app = express();
+const Tweet = require('./models/tweet');
 
 app.listen(3000,async ()=>{
     console.log('Server Started');
     await connect();
     console.log('MongoDb connected');
+
+    const tweet = await Tweet.create({
+        content:"MY third tweet",
+        userEmail:"pp@tweeter.com"
+    })
+    console.log(tweet);
 
 })
