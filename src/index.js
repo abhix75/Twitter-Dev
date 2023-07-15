b14cb1ff5729faec87c145bb3c275f27d966eae7
Abhijit Mishra <abhijitmishraak10@gmail.com>
2023-07-15 22:43:02 +0530

    we have Bulk inserted the Hashtags to the databases collection hashtag

--- src/index.js
+++ src/index.js
@@ -1,15 +1,36 @@
 const express = require("express");
 const connect = require("./config/database");
 const app = express();
-const Tweet = require('./models/tweet')
+const Tweet = require("./models/tweet");
+const Hashtag = require("./models/Hashtag");
+const { HashtagRepository } = require("./repository");
 
 app.listen(3000, async () => {
   console.log("Server Started");
   await connect();
   console.log("MongoDb connected");
-  const tweets = await Tweet.find({
-    content: ["Abhijit is a Nice Guy"],
-  });
-
-  console.log(tweets);
+  const repo = new HashtagRepository();
+  const hashtags = await repo.bulkCreate([
+    {
+      title: "Trend",
+      tweets: [],
+    },
+    {
+      title: "Excited",
+      tweets: [],
+    },
+    {
+      title: "Python",
+      tweets: [],
+    },
+    {
+      title: "Fun",
+      tweets: [],
+    },
+    {
+      title: "Carrer",
+      tweets: [],
+    },
+  ]);
+  console.log(hashtags);
 });
