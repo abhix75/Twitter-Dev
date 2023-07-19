37a2e3bb5821e0db81a1f3e4e497e3908855dd6a
Abhijit Mishra <abhijitmishraak10@gmail.com>
2023-07-19 16:19:29 +0530

    onlys shows the title

--- src/repository/hashtag-repository.js
+++ src/repository/hashtag-repository.js
@@ -39,7 +39,7 @@ class HashtagRepository {
     try {
       const tags = await Hashtag.find({
         title: titleList,
-      }).select('title');
+      }).select('title -_id');
       return tags;
     } catch (error) {
       console.log(error);
