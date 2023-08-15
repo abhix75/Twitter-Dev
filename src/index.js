bb56e30762a7ff78dd2ce19b5e6cdac53912fd6f
Abhijit Mishra <abhijitmishraak10@gmail.com>
2023-08-16 01:02:06 +0530

    updated

--- src/index.js
+++ src/index.js
@@ -17,10 +17,10 @@ app.listen(5000, async () => {
         await connect();
         console.log("MongoDb connected");
 
-        const UserRepo = new UserRepository();
-        const tweetRepo = new TweetRepository();
-        const tweets = await tweetRepo.getAll(0,10);
-        const users = await UserRepo.getAll();
-        const likeService = new LikeService();
-        await likeService.toggleLike(tweets[0].id,'Tweet',users[0].id)
+        // const UserRepo = new UserRepository();
+        // const tweetRepo = new TweetRepository();
+        // const tweets = await tweetRepo.getAll(0,10);
+        // const users = await UserRepo.getAll();
+        // const likeService = new LikeService();
+        // await likeService.toggleLike(tweets[0].id,'Tweet',users[0].id)
 });
